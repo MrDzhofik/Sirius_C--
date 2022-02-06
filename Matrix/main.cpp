96d6c0cade4f1c4e9b8e6c449c8ff8f79758578b
MrDzhofik <djafarovemil04@mail.ru>
2022-02-06 23:12:40 +0300

    Second chance. Day 6. Module Matrix have been finished

--- Matrix/main.cpp
+++ Matrix/main.cpp
@@ -5,25 +5,73 @@ using namespace std;
 
 int main()
 {
-    int n, m;
-    cin >> n >> m;
-    vector<vector<int> > t(n, vector<int> (m, 0));
-    for (int i = 0; i < n; ++i){
-        for (int j = 0; j < m; ++j){
-            if ((i + j) % 2 == 0){
-                t[i][j] = 1;
-            }
-            else{
-                t[i][j] = 0;
+    int n, temp, tmp;
+    cin >> n;
+    vector<vector<int> > t(n, vector<int> (n, 0));
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            cin >> t[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            if ((i == j) && (j <= n)){
+                temp = t[i][i];
+                tmp = t[n - i - 1][i];
+                t[i][i] = tmp;
+                t[n - i - 1][i] = temp;
             }
         }
     }
-    for (int i = 0; i < n; ++i){
-        for (int j = 0; j < m; ++j){
+    for (int i = 0; i < t.size(); ++i){
+        for (int j = 0; j < t[i].size(); ++j){
             cout << t[i][j] << " ";
         }
         cout << endl;
     }
+//    for (int i = 0; i < n; i++){
+//        for (int j = 0; j < n; j++){
+//            if (j <= i){
+//                temp = t[i][j];
+//                t[i][j] = t[j][i];
+//                t[j][i] = temp;
+//                cout << t[i][j] << endl;
+//            }
+//    }
+//    }
+//    for (int i = 0; i < t.size(); ++i){
+//        for (int j = 0; j < t[i].size(); ++j){
+//            cout << t[i][j] << " ";
+//        }
+//        cout << endl;
+//    }
+//    maxi = -10000000;
+//    for (int i = 0; i < n; i++){
+//        for (int j = 0; j < m; j++){
+//            if (t[i][j] > maxi){
+//                maxi = t[i][j];
+//                imax = i;
+//                jmax = j;
+//            }
+//        }
+//    }
+//    cout << imax << ' ' << jmax;
+//    for (int i = 0; i < n; ++i){
+//        for (int j = 0; j < m; ++j){
+//            if ((i + j) % 2 == 0){
+//                t[i][j] = 1;
+//            }
+//            else{
+//                t[i][j] = 0;
+//            }
+//        }
+//    }
+//    for (int i = 0; i < n; ++i){
+//        for (int j = 0; j < m; ++j){
+//            cout << t[i][j] << " ";
+//        }
+//        cout << endl;
+//    }
 //    int n, m;
 //    cin >> n >> m;
 //    vector<vector<int> > t(n, vector<int> (m, 0));
